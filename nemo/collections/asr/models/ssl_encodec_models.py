4797e3f92ff7b3d6a4bcacb70baa726462269292
tbartley94 <tbartley@nvidia.com>
2023-06-13 08:15:10 -0700

    Fixing up code to be cleaner
    Signed-off-by: tbartley94 <tbartley@nvidia.com>

--- nemo/collections/asr/models/ssl_encodec_models.py
+++ nemo/collections/asr/models/ssl_encodec_models.py
@@ -36,20 +36,16 @@ class SpeechEncDecEnCodecSelfSupervisedModel(SpeechEncDecSelfSupervisedModel):
         super().__init__(cfg=cfg, trainer=trainer)
         self.codebook_size = self._cfg.model_defaults.codebook_size
         self.n_codebooks = self._cfg.model_defaults.n_codebooks_to_use
-        self.n_decoders = self._cfg.model_defaults.get("n_decoders_to_use", self.n_codebooks)
-        self.target_heads =  self._cfg.model_defaults.get("target_heads", None)
-        if self.target_heads is not None:
-            self.heads = nn.ModuleList([nn.Linear(self._cfg.decoder_out, self.codebook_size) for _ in range(len(self.target_heads))])
-        else:
-            self.always_first = self._cfg.model_defaults.get("always_first", False)
-            self.never_first = self._cfg.model_defaults.get("never_first", False)
-            assert not (self.always_first and self.never_first)
-            if self.never_first or self.always_first:
-                self.n_decoders = min(self.n_decoders, self.n_codebooks - 1) # can't use more than we have.
-            if self.n_decoders < 1: # Don't need multiple decoders if it's definite.
-                self.heads = nn.ModuleList([nn.Linear(self._cfg.decoder_out, self.codebook_size)])
-            else:
-                self.heads = nn.ModuleList([nn.Linear(self._cfg.decoder_out, self.codebook_size) for _ in range(self.n_codebooks)])
+        self.heads = nn.ModuleList([nn.Linear(self._cfg.decoder_out, self.codebook_size) for _ in range(self.n_codebooks)])
+
+        # Checks which heads to include and exclude. If passed, n_decoders is treated as 'additional' decoders.
+        exclude_codes = self._cfg.model_defaults.get("exclude_codes", [])
+        target_codes =  self._cfg.model_defaults.get("target_codes", [])
+        valid_codes = [idx for idx in range(self.n_codebooks) if idx not in target_codes and idx not in exclude_codes]
+
+        self.valid_codes = valid_codes
+        self.target_codes = target_codes
+        self.n_decoders = self._cfg.model_defaults.get("n_decoders_to_use", len(valid_codes))
 
         if self._cfg.preprocessor.get("init_from_encodec", None):
             logging.info("Copying over Encodec parameters.")
@@ -65,7 +61,6 @@ class SpeechEncDecEnCodecSelfSupervisedModel(SpeechEncDecSelfSupervisedModel):
             augmentor = None
 
         shuffle = config['shuffle']
-
         # Instantiate tarred dataset loader or normal dataset loader
         if config.get('is_tarred', False):
             if ('tarred_audio_filepaths' in config and config['tarred_audio_filepaths'] is None) or (
@@ -221,45 +216,23 @@ class SpeechEncDecEnCodecSelfSupervisedModel(SpeechEncDecSelfSupervisedModel):
 
         outputs = self.decoder_ssl(encoder_output=encoded)
         # -> BxTxD
-        if self.target_heads is not None:
-            selected_heads = self.target_heads
-            denom = len(selected_heads)
-            for i, idx in enumerate(selected_heads):
-                logits = self.heads[i](outputs)
-                curr_loss = self.loss(
-                    spec_masks=spec_masks,
-                    decoder_outputs=nn.functional.log_softmax(logits, -1),
-                    targets=spectrograms[:,idx,:] - self.codebook_size*idx, # since encodings are scaled
-                    decoder_lengths=None,
-                    target_lengths=None,
-                )
-                loss_val_dict[f"head_{idx}"] = curr_loss
-                loss_value = loss_value + curr_loss
+
+        if self.training:
+            selected_heads = self.target_codes + random.sample(self.valid_codes, self.n_decoders)
         else:
-            if self.training:
-                if self.always_first:
-                    selected_heads = [0] + random.sample(range(1, self.n_codebooks), self.n_decoders)
-                elif self.never_first:
-                    selected_heads = random.sample(range(1, self.n_codebooks), self.n_decoders)
-                else:
-                    selected_heads = random.sample(range(self.n_codebooks), self.n_decoders)
-            else:
-                if self.n_decoders >= 1: # We have at least random training so it's fair for evaluation.
-                    selected_heads = range(int(self.never_first), self.n_codebooks)
-                else:
-                    selected_heads = [0]
-            denom = len(selected_heads)
-            for idx in selected_heads:
-                logits = self.heads[idx](outputs)
-                curr_loss = self.loss(
-                    spec_masks=spec_masks,
-                    decoder_outputs=nn.functional.log_softmax(logits, -1),
-                    targets=spectrograms[:,idx,:] - self.codebook_size*idx, # since encodings are scaled
-                    decoder_lengths=None,
-                    target_lengths=None,
-                )
-                loss_val_dict[f"head_{idx}"] = curr_loss
-                loss_value = loss_value + curr_loss
+            selected_heads = self.target_codes + self.valid_codes
+        denom = len(selected_heads)
+        for idx in selected_heads:
+            logits = self.heads[idx](outputs)
+            curr_loss = self.loss(
+                spec_masks=spec_masks,
+                decoder_outputs=nn.functional.log_softmax(logits, -1),
+                targets=spectrograms[:,idx,:] - self.codebook_size*idx, # since encodings are scaled
+                decoder_lengths=None,
+                target_lengths=None,
+            )
+            loss_val_dict[f"head_{idx}"] = curr_loss
+            loss_value = loss_value + curr_loss
         return loss_value/denom, loss_val_dict
 
     def validation_step(self, batch, batch_idx, dataloader_idx=0):
@@ -281,3 +254,11 @@ class SpeechEncDecEnCodecSelfSupervisedModel(SpeechEncDecSelfSupervisedModel):
         del self._in_validation_step
         loss_val_dict["val_loss"] = loss_value
         return loss_val_dict
+
+    # PTL-specific methods
+    def multi_validation_epoch_end(self, outputs, dataloader_idx: int = 0):
+        loss_dict = {}
+        for key in outputs[0].keys():
+            loss_dict[key] = torch.stack([x[key] for x in outputs]).mean()
+        loss_dict['log'] = {'val_loss' : loss_dict['val_loss']}
+        return loss_dict
