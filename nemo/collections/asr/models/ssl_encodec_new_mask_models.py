hoping this fixes masking
Signed-off-by: tbartley94 <tbartley@nvidia.com>

nemo/collections/asr/models/ssl_encodec_new_mask_models.py
@@ -34,7 +34,7 @@ from nemo.core.neural_types import (
     SpectrogramType,
 )
 
-from nemo.collections.asr.models.ssl_models import SpeechEncDecSelfSupervisedModel
+from nemo.collections.asr.models.ssl_encodec_models import SpeechEncDecEnCodecSelfSupervisedModel
 from nemo.collections.asr.data import audio_to_text_dataset
 from nemo.collections.asr.parts.preprocessing.perturb import process_augmentations
 from nemo.core.classes.common import typecheck
@@ -42,80 +42,13 @@ from nemo.core.classes.common import typecheck
 from nemo.utils import logging
 
 
-class SpeechEncDecEnCodecNewMaskSelfSupervisedModel(SpeechEncDecSelfSupervisedModel):
+class SpeechEncDecEnCodecNewMaskSelfSupervisedModel(SpeechEncDecEnCodecSelfSupervisedModel):
     """Base class for encoder-decoder models used for self-supervised encoder pre-training"""
 
     def __init__(self, cfg: DictConfig, trainer: Trainer = None):
+        cfg.model_defaults.decoding_mode = "base"
         super().__init__(cfg=cfg, trainer=trainer)
-        self.codebook_size = self._cfg.model_defaults.codebook_size
-        self.n_codebooks = self._cfg.model_defaults.n_codebooks_to_use
-        # Checks which heads to include and exclude. If passed, n_decoders is treated as 'additional' decoders.
-        exclude_codes = self._cfg.model_defaults.get("exclude_codes", [])
-        target_codes =  self._cfg.model_defaults.get("target_codes", [])
-        valid_codes = [idx for idx in range(self.n_codebooks) if idx not in target_codes and idx not in exclude_codes]
 
-        self.valid_codes = valid_codes
-        self.target_codes = target_codes
-        self.n_decoders = self._cfg.model_defaults.get("n_decoders_to_use", len(valid_codes))
-
-        self._cfg.decoder.feat_out = self.codebook_size
-        self.heads = nn.ModuleList([self.from_config_dict(self._cfg.decoder) for _ in range(self.n_codebooks)])
-        self.decoder_ssl = None
-
-    def _setup_dataloader_from_config(self, config: Optional[Dict]):
-        if 'augmentor' in config:
-            augmentor = process_augmentations(config['augmentor'])
-        else:
-            augmentor = None
-
-        shuffle = config['shuffle']
-        # Instantiate tarred dataset loader or normal dataset loader
-        if config.get('is_tarred', False):
-            if ('tarred_audio_filepaths' in config and config['tarred_audio_filepaths'] is None) or (
-                'manifest_filepath' in config and config['manifest_filepath'] is None
-            ):
-                logging.warning(
-                    "Could not load dataset as `manifest_filepath` was None or "
-                    f"`tarred_audio_filepaths` is None. Provided config : {config}"
-                )
-                return None
-
-            shuffle_n = config.get('shuffle_n', 4 * config['batch_size']) if shuffle else 0
-            dataset = audio_to_text_dataset.get_tarred_dataset(
-                config=config,
-                shuffle_n=shuffle_n,
-                global_rank=self.global_rank,
-                world_size=self.world_size,
-                augmentor=augmentor,
-            )
-            shuffle = False
-        else:
-            if 'manifest_filepath' in config and config['manifest_filepath'] is None:
-                logging.warning(f"Could not load dataset as `manifest_filepath` was None. Provided config : {config}")
-                return None
-            if config.get('audio_type', 'not_codes') == 'codes':
-                dataset = audio_to_text_dataset.get_audioCodes_to_text_char_dataset(config=config, augmentor=augmentor)
-            else:
-                dataset = audio_to_text_dataset.get_char_dataset(config=config, augmentor=augmentor)
-
-        if hasattr(dataset, 'collate_fn'):
-            collate_fn = dataset.collate_fn
-        elif hasattr(dataset.datasets[0], 'collate_fn'):
-            # support datasets that are lists of entries
-            collate_fn = dataset.datasets[0].collate_fn
-        else:
-            # support datasets that are lists of lists
-            collate_fn = dataset.datasets[0].datasets[0].collate_fn
-
-        return torch.utils.data.DataLoader(
-            dataset=dataset,
-            batch_size=config['batch_size'],
-            collate_fn=collate_fn,
-            drop_last=config.get('drop_last', False),
-            shuffle=shuffle,
-            num_workers=config.get('num_workers', 0),
-            pin_memory=config.get('pin_memory', False),
-        )
 
     @property
     def input_types(self) -> Optional[Dict[str, NeuralType]]:
@@ -222,6 +155,7 @@ class SpeechEncDecEnCodecNewMaskSelfSupervisedModel(SpeechEncDecSelfSupervisedMo
         loss_value = encoded.new_zeros(1)
         # -> BxTxD
         denom = len(selected_heads)
+        print(selected_heads)
         for idx in selected_heads:
             logits = self.heads[idx](encoder_output=encoded)
             curr_loss = self.loss(
@@ -259,14 +193,13 @@ class SpeechEncDecEnCodecNewMaskSelfSupervisedModel(SpeechEncDecSelfSupervisedMo
 
         if self.feat_pen:
             loss_value += self.feat_pen
-
         return {'loss': loss_value, 'log': tensorboard_logs}
 
     def validation_step(self, batch, batch_idx, dataloader_idx=0):            
         # Set flag to register tensors
         self._in_validation_step = True
         signal, signal_len, targets, target_lengths = batch
-        spectrograms, spec_masks, encoded, encoded_len, target_codes = self.forward(
+        spectrograms, spec_masks, encoded, encoded_len, _ = self.forward(
                 input_signal=signal, input_signal_length=signal_len,
             )
         loss_value, loss_val_dict = self.decoder_loss_step(spectrograms=spectrograms, spec_masks=spec_masks, encoded=encoded, encoded_len=encoded_len, targets=targets, target_lengths=target_lengths, selected_heads=self.target_codes + self.valid_codes)
@@ -276,10 +209,3 @@ class SpeechEncDecEnCodecNewMaskSelfSupervisedModel(SpeechEncDecSelfSupervisedMo
         loss_val_dict["val_loss"] = loss_value
         return loss_val_dict
 
-    # PTL-specific methods
-    def multi_validation_epoch_end(self, outputs, dataloader_idx: int = 0):
-        loss_dict = {}
-        for key in outputs[0].keys():
-            loss_dict[key] = torch.stack([x[key] for x in outputs]).mean()
-        loss_dict['log'] = {k: v for k, v in loss_dict.items()}
-        return loss_dict